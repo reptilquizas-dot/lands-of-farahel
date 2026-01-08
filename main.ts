76257e6de73a41b875e034289437f57630bf8b19
reptilquizas-dot <reptilquizas@gmail.com>
2026-01-08 18:38:17 +0100

    Actualizar main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -422,23 +422,6 @@ let location = sprites.create(img`
     `, SpriteKind.Food)
 location.setPosition(25, 32)
 scene.cameraFollowSprite(main)
-main = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
+main = sprites.create(assets.image`miImagen4`, SpriteKind.Player)
 controller.moveSprite(main)
 tiles.setTileAt(tiles.getTileLocation(20, 7), sprites.dungeon.hazardSpike)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -12,6 +12,8 @@ namespace myTiles {
     export const tile4 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -70,6 +72,8 @@ namespace myTiles {
             case "tile4":return tile4;
             case "miMosaico3":
             case "tile5":return tile5;
+            case "miMosaico4":
+            case "tile6":return tile6;
         }
         return null;
     })
